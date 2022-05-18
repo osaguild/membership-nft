f482425ecb6b647fdad727636d912bf06ae9b5e2
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-18 10:49:45 +0900

    connect success

--- frontend/lib/web3.ts
+++ frontend/lib/web3.ts
@@ -1,3 +1,18 @@
-export default function connectWallet() {
+import { ethers } from "ethers";
+
+export default async function connectWallet() {
   alert("connect wallet is called");
+
+  try {
+    if (!window.ethereum) {
+      alert("you need metamask to use Dapp");
+      return;
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts", []);
+    console.log("Connected", accounts[0]);
+    //this.currentAccount = accounts[0];
+  } catch (error) {
+    console.log("reject your connection request", error);
+  }
 }
