b49e868bfabc28f4da96720663e05f7ce995917e
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-28 17:08:16 +0900

    update checker

--- backend/contracts/Checker.sol
+++ backend/contracts/Checker.sol
@@ -18,19 +18,15 @@ contract Checker {
      * @dev Set question text and answer
      * @param string text - The text of question
      * @param bool answer - The answer of question
-     * @return uint256 id - The id of question
      */
-    function setQuestion(string memory text, bool answer)
-        external
-        returns (uint256 id)
-    {
+    function setQuestion(string memory text, bool answer) external {
         _totalCount.increment();
         uint256 _id = _totalCount.current();
         _questions[_id].text = text;
         _questions[_id].answer = answer;
         _questions[_id].isActive = true;
 
-        return id;
+        emit QuestionAdded(msg.sender, _id);
     }
 
     /*
@@ -47,9 +43,8 @@ contract Checker {
         if (_questions[id].isActive) {
             return (true, _questions[id].text);
         } else {
-            return (false, "Question is not active");
+            return (false, "Question is not active or out of range");
         }
-        //return (false, "Id is out of range");
     }
 
     /*
@@ -65,32 +60,50 @@ contract Checker {
      * @dev Check answers of question
      * @param uint256[] ids - The id of the question to check
      * @param bool[] answers - The answer of the question
+     * @return bool isCorrect - True if the answers are correct, false otherwise
      * @return string message - The message about check answers
      */
-    function checkAnswers(uint256[] memory ids, bool[] memory answers) public view returns (bool isCollect, string memory message) {
-        // check1: your answers are correct
+    function checkAnswers(uint256[] memory ids, bool[] memory answers)
+        public
+        view
+        returns (bool isCollect, string memory message)
+    {
+        // check1: format is wrong
+        if (ids.length == 0 || answers.length == 0) {
+            return (false, "Format is wrong");
+        }
+        if (ids.length != answers.length) {
+            return (false, "Format is wrong");
+        }
+
+        // check2: answer is wrong
         for (uint256 i = 0; i < ids.length; i++) {
             if (_questions[ids[i]].isActive) {
                 if (_questions[ids[i]].answer != answers[i]) {
-                    return (false, "Your answers are incorrect");
+                    return (false, "Answer is wrong");
                 }
             }
         }
-        // check2: all active questions are answered
+        // check3: all active questions are answered
         for (uint256 i = 1; i <= _totalCount.current(); i++) {
             if (_questions[i].isActive) {
                 bool answered = false;
                 for (uint256 j = 0; j < ids.length; j++) {
                     if (i == ids[j]) {
                         answered = true;
-                        break; 
+                        break;
                     }
                 }
                 if (!answered) {
-                    return (false, "You have not answered all active questions");
+                    return (
+                        false,
+                        "You have not answered all active questions"
+                    );
                 }
             }
         }
-        return (true, "All answers are correct");
+        return (true, "Correct answer");
     }
+
+    event QuestionAdded(address sender, uint256 id);
 }
--- backend/test/test.ts
+++ backend/test/test.ts
@@ -1,48 +1,214 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
-describe("ALL", async function () {
+describe("NFT.sol", async function () {
+
+  let owner: any;
+  let user1: any;
+  let user2: any;
+  let nft: any;
+
+  before(async function () {
+    // accounts
+    [owner, user1, user2] = await ethers.getSigners();
+
+    // deploy NFT
+    const NFT = await ethers.getContractFactory("NFT");
+    nft = await NFT.deploy();
+
+  });
+
+  it("mint", async function () {
+
+    // mint and check balance
+    await nft.mint(user1.address, "test");
+    expect(await nft.balanceOf(user1.address)).to.equal(1);
+    expect(await nft.balanceOf(owner.address)).to.equal(0);
+
+  });
+
+});
+
+describe("Manager.sol", async function () {
 
   let owner: any;
   let user1: any;
   let user2: any;
   let nft: any;
   let manager: any;
-  let checker: any;
 
   before(async function () {
+
+    // accounts
     [owner, user1, user2] = await ethers.getSigners();
+
+    // deploy NFT
     const NFT = await ethers.getContractFactory("NFT");
     nft = await NFT.deploy();
+
+    // deploy Manager
     const Manager = await ethers.getContractFactory("Manager");
     manager = await Manager.deploy();
-    const Checker = await ethers.getContractFactory("Checker");
-    checker = await Checker.deploy();
+
   });
 
-  it("mint", async function () {
-    await nft.mint(user1.address, "test");
+  it("add member", async function () {
+
+    // mint and check balance
+    await manager.addMember(nft.address, user1.address);
     expect(await nft.balanceOf(user1.address)).to.equal(1);
     expect(await nft.balanceOf(owner.address)).to.equal(0);
+
   });
 
-  it("mint from Manager", async function () {
-    await nft.setApprovalForAll(manager.address, true);
-    await manager.addMember(nft.address, user1.address);
-    expect(await nft.balanceOf(user1.address)).to.equal(2);
-    expect(await nft.balanceOf(owner.address)).to.equal(0);
+});
+
+
+describe("Checker.sol", async function () {
+
+  let owner: any;
+  let user1: any;
+  let user2: any;
+  let checker: any;
+  const text1 = "Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.";
+  const text2 = "No spam or self-promotion (server invites, advertisements, etc) without permission from an admin. This includes DMing fellow members.";
+  const answer1 = true;
+  const answer2 = false;
+
+  before(async function () {
+
+    // accounts
+    [owner, user1, user2] = await ethers.getSigners();
+
+    // deploy Checker
+    const Checker = await ethers.getContractFactory("Checker");
+    checker = await Checker.deploy();
+
   });
 
-  it("question 1", async function () {
-    expect(await checker.getQuestion(0)).to.equal("Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.");
-    expect(await checker.checkAnswer(0, true)).to.equal(true);
-    expect(await checker.checkAnswer(0, false)).to.equal(false);
+  it("before set question", async function () {
+
+    // count of question
+    expect(await checker.getCountOfQuestions()).to.equal(0);
+
+    // question 0
+    const [isCollectId0, data0] = await checker.getQuestion(0);
+    expect(isCollectId0).to.equal(false);
+    expect(data0).to.equal("Question is not active or out of range");
+
+    // question 1
+    const [isCollectId1, data1] = await checker.getQuestion(1);
+    expect(isCollectId1).to.equal(false);
+    expect(data1).to.equal("Question is not active or out of range");
+
+    // question 2
+    const [isCollectId2, data2] = await checker.getQuestion(2);
+    expect(isCollectId2).to.equal(false);
+    expect(data2).to.equal("Question is not active or out of range");
+
   });
 
-  it("question 2", async function () {
-    expect(await checker.getQuestion(1)).to.equal("No spam or self-promotion (server invites, advertisements, etc) without permission from an admin. This includes DMing fellow members.");
-    expect(await checker.checkAnswer(1, true)).to.equal(true);
-    expect(await checker.checkAnswer(1, false)).to.equal(false);
+  it("set question 1", async function () {
+
+    // set question
+    await expect(await checker.setQuestion(text1, answer1)).to.emit(checker, "QuestionAdded").withArgs(owner.address, 1);
+
+    // count of question
+    expect(await checker.getCountOfQuestions()).to.equal(1);
+
+    // question 0
+    const [isCollectId0, data0] = await checker.getQuestion(0);
+    expect(isCollectId0).to.equal(false);
+    expect(data0).to.equal("Question is not active or out of range");
+
+    // question 1
+    const [isCollectId1, data1] = await checker.getQuestion(1);
+    expect(isCollectId1).to.equal(true);
+    expect(data1).to.equal(text1);
+
+    // question 2
+    const [isCollectId2, data2] = await checker.getQuestion(2);
+    expect(isCollectId2).to.equal(false);
+    expect(data2).to.equal("Question is not active or out of range");
+
+  });
+
+  it("set question 2", async function () {
+
+    // set question
+    await expect(await checker.setQuestion(text2, answer2)).to.emit(checker, "QuestionAdded").withArgs(owner.address, 2);
+
+    // count of question
+    expect(await checker.getCountOfQuestions()).to.equal(2);
+
+    // question 0
+    const [isCollectId0, data0] = await checker.getQuestion(0);
+    expect(isCollectId0).to.equal(false);
+    expect(data0).to.equal("Question is not active or out of range");
+
+    // question 1
+    const [isCollectId1, data1] = await checker.getQuestion(1);
+    expect(isCollectId1).to.equal(true);
+    expect(data1).to.equal(text1);
+
+    // question 2
+    const [isCollectId2, data2] = await checker.getQuestion(2);
+    expect(isCollectId2).to.equal(true);
+    expect(data2).to.equal(text2);
+
+  });
+
+  it("check answers", async function () {
+
+    // empty ids
+    const ids0: number[] = [];
+    const answers0: boolean[] = [true, false];
+    const [isCollect0, message0] = await checker.checkAnswers(ids0, answers0);
+    expect(isCollect0).to.equal(false);
+    expect(message0).to.equal("Format is wrong");
+
+    // empty answers
+    const ids1: number[] = [1, 2];
+    const answers1: boolean[] = [];
+    const [isCollect1, message1] = await checker.checkAnswers(ids1, answers1);
+    expect(isCollect1).to.equal(false);
+    expect(message1).to.equal("Format is wrong");
+
+    // length of ids and answers are different
+    const ids2: number[] = [1];
+    const answers2: boolean[] = [true, false];
+    const [isCollect2, message2] = await checker.checkAnswers(ids2, answers2);
+    expect(isCollect2).to.equal(false);
+    expect(message2).to.equal("Format is wrong");
+
+    // wrong answer
+    const ids3: number[] = [1, 2];
+    const answers3: boolean[] = [true, true];
+    const [isCollect3, message3] = await checker.checkAnswers(ids3, answers3);
+    expect(isCollect3).to.equal(false);
+    expect(message3).to.equal("Answer is wrong");
+
+    // not enouth answers
+    const ids4: number[] = [2];
+    const answers4: boolean[] = [false];
+    const [isCollect4, message4] = await checker.checkAnswers(ids4, answers4);
+    expect(isCollect4).to.equal(false);
+    expect(message4).to.equal("You have not answered all active questions");
+
+    // too much answers
+    const ids5: number[] = [1, 2, 3];
+    const answers5: boolean[] = [true, false, true];
+    const [isCollect5, message5] = await checker.checkAnswers(ids5, answers5);
+    expect(isCollect5).to.equal(true);
+    expect(message5).to.equal("Correct answer");
+
+    // collect answer
+    const ids6: number[] = [1, 2];
+    const answers6: boolean[] = [true, false];
+    const [isCollect6, message6] = await checker.checkAnswers(ids6, answers6);
+    expect(isCollect6).to.equal(true);
+    expect(message6).to.equal("Correct answer");
+
   });
 
 });
