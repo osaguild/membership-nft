24058a56f7c9fa1fcf4e9c1c260d5ec7d13a4bce
osaguild <nobuhisa.abe.0922@gmail.com>
2022-05-30 08:21:27 +0900

    update script

--- backend/scripts/deploy_checker.ts
+++ backend/scripts/deploy_checker.ts
@@ -1,12 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  // deploy
   const [deployer] = await ethers.getSigners();
   console.log("deploying contract with the account:", deployer.address);
   console.log("account balance:", (await deployer.getBalance().toString()));
   const Checker = await ethers.getContractFactory("Checker");
   const checker = await Checker.deploy();
   console.log("Checker address:", checker.address);
+  // set questions
+  await checker.setQuestion("Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.", true);
+  await checker.setQuestion("No spam or self-promotion (server invites, advertisements, etc) without permission from an admin. This includes DMing fellow members.", true);
+  const q1 = await checker.getQuestion(1);
+  console.log("q1:", q1);
+  const q2 = await checker.getQuestion(2);
+  console.log("q2:", q2);
 }
 
 main()
